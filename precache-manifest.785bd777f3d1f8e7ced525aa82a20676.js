f8f600c7da91477c35aec8ccf132c869fc797345
Jitrapon-poonsuk <jitraponpoonsuk@gmail.com>
2020-04-08 01:03:44 +0700

    Updates

---
+++ precache-manifest.785bd777f3d1f8e7ced525aa82a20676.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba7dcbca36539f8099f5b01fcb83eda",
+    "url": "/react_tic_tac_toe/index.html"
+  },
+  {
+    "revision": "203487f5aac972496580",
+    "url": "/react_tic_tac_toe/static/js/2.02e04f20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_tic_tac_toe/static/js/2.02e04f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35a7368f4f7d12a46890",
+    "url": "/react_tic_tac_toe/static/js/main.50282a59.chunk.js"
+  },
+  {
+    "revision": "285406879cfea6b2e124",
+    "url": "/react_tic_tac_toe/static/js/runtime-main.07c8008d.js"
+  }
+]);
